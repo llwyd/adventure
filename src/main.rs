15428e1dbce3bf3be78a26b0763323a714737cfb
llwyd <tom@llwyd.io>
2024-06-22 20:24:09 +0100

    serialising toml

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
 use std::io;
-use std::io::{Write};
+use std::io::prelude::*;
+use std::fs::File;
+use std::collections::HashMap;
+use toml::{Value, de::Error};
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize, Debug)]
+struct State{
+    dialogue: String,
+    action: Vec<Action>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Action{
+    action: String,
+    next_state: u32,
+}
 
 fn main() {
-    println!("Hello, world!");
+
+    /* Load game */
+    let mut file = File::open("assets/story.toml").expect("Failed to read file");
+    let mut story_str = String::new();
+    file.read_to_string(&mut story_str).expect("Failed to read story as string");
+   
+    //let story:Value = toml::from_str(&story_str).expect("Failed to deserialize toml");
+    let story:HashMap<String, Vec<State>> = toml::from_str(&story_str).expect("Failed to deserialize toml");
+    /* TODO, prettier printing */
+
+    println!("---META START---");
+    println!("{:?}", story);
+    println!("----META END----");
+    println!("");
+
+    println!("{:?}", story["state"][0].dialogue);
+    //println!("{:?}", story["state"][0]["dialogue"]);
 
     print!("> ");
     let _ = io::stdout().flush();
