d6c0caef5a3a457e50ba44bfc9c6b7b016794db0
llwyd <tom@llwyd.io>
2024-06-22 14:11:12 +0100

    basic io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use std::io::{Write};
+
 fn main() {
     println!("Hello, world!");
+
+    print!("> ");
+    let _ = io::stdout().flush();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read command :(");
+    let command = input.trim();
+
+    match command{
+        "hello" => println!("Hello!"),
+        _ => {},
+    }
 }
